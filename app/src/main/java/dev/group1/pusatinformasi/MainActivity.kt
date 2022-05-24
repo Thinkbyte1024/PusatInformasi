3a5abd4caec654e291e35d8e1aec86f5e9113857
Muhammad Aditya <muhammad_aditya_01@icloud.com>
2022-05-24 10:12:02 +0700

    Homepage modification

--- app/src/main/java/dev/group1/pusatinformasi/MainActivity.kt
+++ app/src/main/java/dev/group1/pusatinformasi/MainActivity.kt
@@ -3,13 +3,16 @@ package dev.group1.pusatinformasi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.group1.pusatinformasi.ui.theme.PusatInformasiTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +20,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PusatInformasiTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Homepage()
                 }
             }
         }
@@ -30,14 +32,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Homepage() {
+    Scaffold(
+        topBar = {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "Homepage",
+                    textAlign = TextAlign.Start,
+                    style = MaterialTheme.typography.h5,
+                    modifier = Modifier.padding(horizontal = 8.dp)
+                )
+                TextButton(modifier = Modifier.padding(8.dp), onClick = {
+
+                }) {
+                    Text(
+                        "Login",
+                        Modifier.padding(horizontal = 8.dp),
+                        textAlign = TextAlign.Start
+                    )
+                    Icon(
+                        modifier = Modifier.size(32.dp),
+                        imageVector = Icons.Default.AccountCircle,
+                        contentDescription = "Login Button"
+                    )
+                }
+            }
+        },
+        bottomBar = {
+
+        }
+    ) {
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     PusatInformasiTheme {
-        Greeting("Android")
+        Homepage()
     }
 }
